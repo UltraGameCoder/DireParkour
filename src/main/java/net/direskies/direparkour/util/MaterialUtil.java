7091e935f8453466a27737e6585722be596851bf
SadGhostYT <idanr2015@gmail.com>
2023-05-29 12:26:53 +0300

    refractor: make pressure plate list immutable

--- src/main/java/net/direskies/direparkour/util/MaterialUtil.java
+++ src/main/java/net/direskies/direparkour/util/MaterialUtil.java
@@ -3,6 +3,7 @@ package net.direskies.direparkour.util;
 import org.bukkit.Material;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,5 +11,11 @@ import java.util.stream.Collectors;
 public class MaterialUtil {
 
     //Future-proof list of pressure plates.
-    public static final Set<Material> PRESSURE_PLATES = EnumSet.copyOf(Arrays.stream(Material.values()).filter(material -> material.name().contains("PRESSURE_PLATE")).collect(Collectors.toList()));
+    public static final Set<Material> PRESSURE_PLATES = Collections.unmodifiableSet(
+            EnumSet.copyOf(
+                    Arrays.stream(Material.values())
+                            .filter(material -> material.name().contains("PRESSURE_PLATE"))
+                            .collect(Collectors.toList())
+            )
+    );
 }
